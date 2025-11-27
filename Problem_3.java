2a482199bf1fa123975eb4f7740b6a49c3bf3978
TANMOY PRAMANICK <pramanickt15@gmail.com>
2025-11-27 13:13:11 +0530

    Create Problem_3.java

---
+++ Problem_3.java
@@ -0,0 +1,30 @@
+class Problem_3 {
+    public static void m(int a) {
+		if (a % 2 == 0) {
+			a--;
+		}
+		int num = 1;
+		for (int i = 1; i <= a; i++) {
+			if (i == a) {
+				System.out.print(num);
+			} else {
+				System.out.print(num + ", ");
+			}
+			num += 2;
+		}
+	}
+    public static void main(String[] args) {
+		m(1);
+		System.out.println();
+		m(2);
+		System.out.println();
+		m(3);
+		System.out.println();
+		m(4);
+		System.out.println();
+		m(5);
+		System.out.println();
+		m(6);
+
+	}
+}
